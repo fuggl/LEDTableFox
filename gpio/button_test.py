7d4228f3ec85a3aea6e275729a10a064da11ae8b
sebastian <fuchs.se.de@gmail.com>
2020-06-30 21:59:46 +0200

    added button push test via extra thread

--- gpio/button_test.py
+++ gpio/button_test.py
@@ -1,10 +1,28 @@
+import time
+from threading import Thread
+
 BUTTON_IDX_LEFT = 1
 BUTTON_IDX_RIGHT = 2
 
 
+def empty_listener(seat_idx, button_idx):
+    print("{} {}".format(seat_idx, button_idx))
+
+
+listener = empty_listener
+
+
 def register(button_callback):
-    return button_callback
+    global listener
+    listener = button_callback
+
+
+def test_button():
+    time.sleep(30)
+    print("test button")
+    listener(1, 1)
 
 
 def init():
+    Thread(target=test_button).start()
     return
